fd657eaa410e820a971c17754f2f261fb44ca4df
Darron Haworth <darron.haworth@firsttechfed.com>
2016-07-12 08:10:36 -0700

    added suport for spread operator

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,19 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var objOne = {
+    name: 'Darron',
+    location: 'Rocklin'
+};
+var objTwo = {
+    age:50,
+    ...objOne
+};
+
+
+
+console.log(objTwo);
+
 ReactDOM.render(
     <h1>Boilerplate app!</h1>,
     document.getElementById('app')
